a59d4321b40a65369ae28d1a00f0f27a39c832bc
Quang Viet <vietgunnyvip0000@gmail.com>
2023-05-16 22:23:13 +0700

    Add the compont StatCard

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -10,6 +10,9 @@ type Props = {
     long:string,
   }
 }
+const results = 7;
+const windspeed =14.8;
+const wind_Direction =315;
 async function WeatherPage({params:{city,lat,long}}:Props) {
   // const client = getClient();
   // const {data}= await client.query({
@@ -35,17 +38,53 @@ async function WeatherPage({params:{city,lat,long}}:Props) {
             Place: {city} : {lat} {' - '} {long}
           </div>
         </div>
-        <div>
+        <div className='m-2 mb-10'>
           <CalloutCard
           message='This is the text for the weather'
            />
         </div>
-        <div>
+        <div className='grid grid-cols-1 xl:grid-cols-2 gap-5'>
           <StatCard
-            tittle='Minium Tempera'
-            metrics="$ 60,465"
+            tittle='Maxium Temperature'
+            metrics="29 °C"
             color='yellow'
           />
+          <StatCard
+            tittle='Minium Temperature'
+            metrics="29 °C"
+            color='green'
+          />
+          <div>
+            <StatCard
+              tittle='UV Index'
+              metrics={`${results.toFixed(1)}°`}
+              color='rose'
+            />
+            {Number(results) > 5 && (
+              <CalloutCard
+                message={"The UV is high today, be sure to wear SPF!"}
+                warning
+              />
+            )}
+          </div>
+          <div className='flex space-x-3'>
+            <StatCard
+              tittle='Wind Speed'
+              metrics={`${windspeed.toFixed(1)}m/s`}
+              color='cyan'
+            />
+            <StatCard
+              tittle='Wind Direction'
+              metrics={`${wind_Direction.toFixed(1)}°`}
+              color='violet'
+            />
+          </div>
+        </div>
+        <hr className='mb-5'/>
+        <div className='space-y-3'>
+          {/* TempChart */}
+          {/* RainChart */}
+          {/* HumidityChart */}
         </div>
       </div>
     </div>
